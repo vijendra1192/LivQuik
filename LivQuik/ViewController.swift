a7d83c1461764062c19c99d119283160b0b1d9b0
vijendra1192 <vijendra.yadav@paynearby.in>
2019-12-10 16:42:34 +0530

    Initial Commit

---
+++ LivQuik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LivQuik
+//
+//  Created by Vijendra Yadav on 10/12/2019.
+//  Copyright © 2019 Vijendra Yadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
