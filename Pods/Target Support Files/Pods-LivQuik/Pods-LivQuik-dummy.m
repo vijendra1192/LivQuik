788243d22e7077450a5493fc38034825b335fc40
vijendra1192 <vijendra.yadav@paynearby.in>
2019-12-12 16:51:33 +0530

    * Final Code with pod update

---
+++ Pods/Target Support Files/Pods-LivQuik/Pods-LivQuik-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LivQuik : NSObject
+@end
+@implementation PodsDummy_Pods_LivQuik
+@end
---
+++ Pods/Target Support Files/Pods-LivQuikTests/Pods-LivQuikTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LivQuikTests : NSObject
+@end
+@implementation PodsDummy_Pods_LivQuikTests
+@end
